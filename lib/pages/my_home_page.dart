b46e19831fe375f8989da1f718eb321e37b5de70
mk-mob <mkmob314@gmail.com>
2024-07-03 14:53:49 +0900

    cloudfirestoreに初期データ保存

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import '../flavors.dart';
 
@@ -9,6 +11,20 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(F.title),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          try {
+            final firstData = {"msg": "成功しました!"};
+            await FirebaseFirestore.instance
+                .collection('data')
+                .doc()
+                .set(firstData);
+            print("書き込みが成功しました");
+          } catch (e) {
+            print("アクセスが拒否されました");
+          }
+        },
       ),
       body: Center(
         child: Text(
