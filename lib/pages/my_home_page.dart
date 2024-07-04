4484f8d8552e1b42b0dfd2520120b4652c82e3fe
mk-mob <mkmob314@gmail.com>
2024-07-04 15:18:08 +0900

    myhomepage_controller作成

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:text_sns/controller/my_homepage_controller.dart';
+
 import '../flavors.dart';
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(MyHomePageController());
     return Scaffold(
       appBar: AppBar(
         title: Text(F.title),
       ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: controller.onFloatingActionButtonPressed),
       body: Center(
         child: Text(
           'Hello ${F.title}',
